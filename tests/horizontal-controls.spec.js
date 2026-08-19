17303a2cc5b0e67961f200470c028bd06963c5fb
Marcos <marcosmallet@gmail.com>
2026-08-19 12:38:32 -0300

    fix: use ESM in horizontal controls test

--- tests/horizontal-controls.spec.js
+++ tests/horizontal-controls.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 test('personagem move para esquerda e direita respeitando os limites', async ({ page }) => {
   await page.goto('/?debug=1');
